f54dfe79b41c38456487b68dd17cb7c53e2158c3
purnema <144817244+purnema@users.noreply.github.com>
2023-11-28 11:10:55 -0500

    working

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -9,24 +9,33 @@ useEffect(() =>{
   axios.get('/api/volume')
   .then(response => {setVolume(response.data.volume);
   });
-
 },[]);
-const handleVolumeChange = (newVolume) => {
 
-    axios.put('/api/volume',{ volume: newVolume })
-    .then(response => {
-      console.log(response.data.message);
-      setVolume(newVolume);
-    });
-  };
+useEffect(() =>{
+  axios.put('/api/volume', {
+    volume: volume
+  })
+  .then(response => {
+    console.log('Volume saved succeffuly:', response.data.volume);
+  });
+
+},[volume]);
+
   return (
     <>
       <div>
       <h1>Settings</h1>
+      <button className ="mb-20" onClick={() => {
+        setVolume(volume+1);
+      }}>Inc Volume</button>
+        <button className ="mb-20" onClick={() => {
+       setVolume(volume-1);
+      }}>Dec Volume</button>
       <p>Volume: {volume}</p>
-       </div>
+      </div>
     </>
   );
 };
 
 export default App;
+
