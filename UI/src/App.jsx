65ea4ec3cabcde10f72f1a675560db5ff03f37ba
purnema <144817244+purnema@users.noreply.github.com>
2023-12-01 01:53:55 -0500

    react-router-dom added

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AudioPlayback from './screens/AudioPlayback';
 
 function App() {
   return (
     <>
       <div>
-      <h1>Settings</h1>
-      <AudioPlayback/>
+      <h1>Music Player</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AudioPlayback />} />
+        </Routes>
+      </Router>
+
       </div>
     </>
   );
