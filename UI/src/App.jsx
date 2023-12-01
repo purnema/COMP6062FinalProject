8a07bd01383c349449fbbf617610ae4253090111
purnema <144817244+purnema@users.noreply.github.com>
2023-11-30 22:03:59 -0500

    AudioPlayback screen

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,16 +1,12 @@
-
-import Volume from './components/volume';
 import './App.css'
+import AudioPlayback from './screens/AudioPlayback';
 
 function App() {
-  
-
   return (
     <>
       <div>
       <h1>Settings</h1>
-      <Volume/>
-
+      <AudioPlayback/>
       </div>
     </>
   );
