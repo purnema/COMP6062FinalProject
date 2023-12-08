320398d3aec54f3615907e0b05841de462905697
purnema <144817244+purnema@users.noreply.github.com>
2023-12-08 17:32:16 -0500

    display updated for all buttons

--- UI/src/components/volume.jsx
+++ UI/src/components/volume.jsx
@@ -1,63 +1,76 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { FaVolumeMute, FaVolumeUp, FaVolumeDown } from "react-icons/fa";
 
 const Volume = () => {
-    const[volume, setVolume]= useState(5);
-    const[mute, setMute] = useState(0);
-  
-  useEffect(() =>{
-    axios.get('/api/volume')
-    .then(response => {setVolume(response.data.volume);
+  const [volume, setVolume] = useState(5);
+  const [mute, setMute] = useState(0);
+
+  useEffect(() => {
+    axios.get("/api/volume").then((response) => {
+      setVolume(response.data.volume);
     });
-  },[]);
-  
-  useEffect(() =>{
-    axios.put('/api/volume', {
-      volume: volume,
-      mute: mute
-    })
-    .then(response => {
-      console.log('System Muted', response.data.mute);
-    });
-  },[volume,mute]);
-  
-    return ( 
-    <div> 
-        <button className ="mb-20" onClick={() => {
-            if(!mute)
-            {
-              if(volume >= 10)
-              {
-                setVolume(10);
-              }
-              else
-              {
-                setVolume(volume+1);
-              }
-            }
-            
-          }}>Inc Volume</button>
-          <button className ="mb-20" onClick={() => {
-            if(!mute)
-            {
-              if(volume <= 0)
-              {
-                setVolume(0);
-              }
-              else
-              {
-                setVolume(volume-1);
-              }
-            }
-    
-          }}>Dec Volume</button>
-          <p>Volume: {mute?"muted":volume}</p>
-          <button className ="mb-20" onClick={() => {
-            setMute(!mute);
-          }}>Mute</button>
-          
+  }, []);
+
+  useEffect(() => {
+    axios
+      .put("/api/volume", {
+        volume: volume,
+        mute: mute,
+      })
+      .then((response) => {
+        console.log("System Muted", response.data.mute);
+      });
+  }, [volume, mute]);
+
+  return (
+    <div>
+      <button
+        className="mb-20 w-24 h-24 rounded-full text-white focus:outline-none bg-blue-500"
+        onClick={() => {
+          if (!mute && volume < 10) {
+            setVolume(volume + 1);
+          }
+        }}
+        style={{ backgroundColor: "	palegreen" }}
+      >
+        <FaVolumeUp className="mr-2" size={20} />
+        Increase Volume
+      </button>
+      <button
+        className="mb-20 w-24 h-24 rounded-full text-white focus:outline-none bg-blue-500"
+        onClick={() => {
+          if (!mute && volume > 0) {
+            setVolume(volume - 1);
+          }
+        }}
+        style={{ backgroundColor: "lavender" }}
+      >
+        <FaVolumeDown className="mr-2" size={20} />
+        Dec Volume
+      </button>
+      <p>Volume: {mute ? "muted" : volume}</p>
+      <button
+        className="mb-20 w-24 h-24 rounded-full text-white focus:outline-none bg-blue-500"
+        onClick={() => {
+          setMute(!mute);
+        }}
+        style={{ backgroundColor: "turquoise" }}
+      >
+        {mute ? (
+          <>
+            <FaVolumeMute className="mr-2" size={20} />
+            Unmute
+          </>
+        ) : (
+          <>
+            <FaVolumeUp className="mr-2" size={20} />
+            Mute
+          </>
+        )}
+      </button>
     </div>
-    );
-}
- 
+  );
+};
+
 export default Volume;
--- UI/src/screens/AudioPlayback.jsx
+++ UI/src/screens/AudioPlayback.jsx
@@ -1,24 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Volume from '../components/volume';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  FaPlay,
+  FaPause,
+  FaStop,
+  FaSyncAlt,
+  FaBan,
+  FaStepForward,
+  FaUndo,
+} from "react-icons/fa";
+import Volume from "../components/volume";
 
 const fetchPlaylist = async () => {
-    try {
-      const response = await fetch('/api/playlist');
-      if (!response.ok) {
-        throw new Error('Failed to fetch playlist');
-      }
-      const data = await response.json();
-      console.log('Fetched playlist:', data.playlist);
-      return data.playlist;
-    } catch (error) {
-      console.error('Error fetching playlist:', error);
-      throw error;
+  try {
+    const response = await fetch("/api/playlist");
+    if (!response.ok) {
+      throw new Error("Failed to fetch playlist");
     }
-  };
+    const data = await response.json();
+    console.log("Fetched playlist:", data.playlist);
+    return data.playlist;
+  } catch (error) {
+    console.error("Error fetching playlist:", error);
+    throw error;
+  }
+};
 
-  const AudioPlayback = () => {
+const AudioPlayback = () => {
   const [playlist, setPlaylist] = useState([]);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [currentSong, setCurrentSong] = useState(null);
@@ -27,24 +35,24 @@ const fetchPlaylist = async () => {
   const audioRef = React.useRef(new Audio());
 
   useEffect(() => {
-    axios.get('http://localhost:5173/api/volume')
-      .then(response => {
+    axios
+      .get("http://localhost:5173/api/volume")
+      .then((response) => {
         console.log(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching volume:', error);
+      .catch((error) => {
+        console.error("Error fetching volume:", error);
       });
   }, []);
 
-    useEffect(() => {
-    fetchPlaylist().then(data => {
-      setPlaylist(data);
-      setCurrentSong(data[currentSongIndex]);
-    })
-    .catch(error => console.error('Error fetching playlist:', error));
+  useEffect(() => {
+    fetchPlaylist()
+      .then((data) => {
+        setPlaylist(data);
+        setCurrentSong(data[currentSongIndex]);
+      })
+      .catch((error) => console.error("Error fetching playlist:", error));
   }, [currentSongIndex]);
-  
-
 
   const togglePlayPause = () => {
     if (isPlaying) {
@@ -95,11 +103,15 @@ const fetchPlaylist = async () => {
   return (
     <>
       <div>
-      {currentSong ? (
+        {currentSong ? (
           <>
             <p>Title: {currentSong.title}</p>
             <p>Artist: {currentSong.artist}</p>
-            <img src={currentSong.albumArt} alt="Album Art" style={{ width: '100px', height: '100px' }} />
+            <img
+              src={currentSong.albumArt}
+              alt="Album Art"
+              style={{ width: "100px", height: "100px" }}
+            />
           </>
         ) : (
           <p>No Current Song</p>
@@ -107,14 +119,57 @@ const fetchPlaylist = async () => {
         <Volume />
       </div>
       <div>
-        <button onClick={togglePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
-        <button onClick={stopPlayback}>Stop</button>
-        <button onClick={toggleLoop}>{isLooping ? 'Disable Loop' : 'Enable Loop'}</button>
-        <button onClick={nextTrack}>Next Track</button>
-        <button onClick={restartTrack}>Restart Track</button>
+        <button
+          className="w-24 h-24 rounded-full focus:outline-none"
+          onClick={togglePlayPause}
+          style={{ backgroundColor: "yellow" }}
+        >
+          {isPlaying ? (
+            <FaPause className="text-white" size={32} />
+          ) : (
+            <FaPlay className="text-white" size={32} />
+          )}
+        </button>
+        <button
+          className="w-24 h-24 rounded-full focus:outline-none"
+          onClick={stopPlayback}
+          style={{ backgroundColor: "lightsalmon" }}
+        >
+          <FaStop className="text-white" size={32} />
+        </button>
+        <button
+          className=" text-white px-4 py-2 rounded"
+          onClick={toggleLoop}
+          style={{ backgroundColor: "moccasin" }}
+        >
+          {isLooping ? (
+            <>
+              <FaBan className="mr-2" /> Disable Loop
+            </>
+          ) : (
+            <>
+              <FaSyncAlt className="mr-2" /> Enable Loop
+            </>
+          )}
+        </button>
+        <button
+          className=" text-white px-4 py-2 rounded"
+          onClick={nextTrack}
+          style={{ backgroundColor: "cornflowerblue" }}
+        >
+          <FaStepForward className="mr-2" /> Next Track
+        </button>
+
+        <button
+          className=" text-white px-4 py-2 rounded"
+          onClick={restartTrack}
+          style={{ backgroundColor: "deeppink" }}
+        >
+          <FaUndo className="mr-2" /> Restart Track
+        </button>
       </div>
     </>
   );
-}
+};
 
 export default AudioPlayback;
